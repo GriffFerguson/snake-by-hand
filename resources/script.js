7f47413c31594445dc55563d3e8f713918856062
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-13 15:51:41 -0500

    Rework positioning
    Rebuild boundaries.
    Maybe add a grid overlay?

--- resources/script.js
+++ resources/script.js
@@ -2,38 +2,38 @@ window.onload = function() {
 var score = 0;
 
 //Player
-var horizontal = 49.5;
+var horizontal = 50;
 var horizontalChange = 0;
-var vertical = 49.5;
+var vertical = 25;
 var verticalChange = 0;
 var player = document.getElementById("player");
 
 //Target
-var targetHori = (Math.random() * 98);
+var targetHori = (Math.round(Math.random() * 95));
 var target = document.getElementById("target");
-var targetVert = (Math.random() * 98);
+var targetVert = (Math.round(Math.random() * 47.5));
 
 document.addEventListener('keydown', function(e) {
     switch(e.code) {
         case "ArrowRight":
             // console.log("right arrow pressed");
-            horizontalChange = 0.6;
+            horizontalChange = 0.5;
             verticalChange = 0;
             break;
         case "ArrowLeft":
             // console.log("left arrow pressed");
-            horizontalChange = -0.6;
+            horizontalChange = -0.5;
             verticalChange = 0;
             break;
         case "ArrowUp":
             // console.log("up arrow pressed");
             horizontalChange = 0;
-            verticalChange = -1;
+            verticalChange = -0.5;
             break;
         case "ArrowDown":
             // console.log("down arrow pressed");
             horizontalChange = 0;
-            verticalChange = 1;
+            verticalChange = 0.5;
             break;
     }
     // console.log("Horizontal: " + horizontal);
@@ -46,10 +46,16 @@ setInterval (function() {
     //Player functions
     horizontal = horizontal + horizontalChange;
     vertical = vertical + verticalChange;
-    player.style.transform = 'translate(' + horizontal + 'vw,' + vertical + 'vh)';  
+    player.style.transform = 'translate(' + horizontal + 'vw,' + vertical + 'vw)';  
+    
+    var gameOver = 0;
     if (horizontal < 0 || horizontal > 100 || vertical < 0 || vertical > 100) {
         document.getElementById("gameOver").style.display = 'block';
         document.getElementById("finalScore").innerText = score;
+        gameOver = 1;
+    }
+
+    if (gameOver == 1) {
         horizontalChange = 0;
         verticalChange = 0;
         horizontal = 49.5;
@@ -57,16 +63,16 @@ setInterval (function() {
     }
 
     //Target functions
-    target.style.transform = 'translate(' + targetHori +'vw, ' + targetVert + 'vh)'
+    target.style.transform = 'translate(' + targetHori +'vw, ' + targetVert + 'vw)'
     if (
-        (horizontal - 2.5) < targetHori &&
-        (horizontal + 2.5) > targetHori &&
-        (vertical + 3.25) > targetVert &&
-        (vertical - 3.25) < targetVert
+        (horizontal - 2) < targetHori &&
+        (horizontal + 2) > targetHori &&
+        (vertical + 2) > targetVert &&
+        (vertical - 2) < targetVert
         ) {
             // console.log("Target hit!")
-            targetVert = (Math.random() * 98);
-            targetHori = (Math.random() * 98);
+            targetVert = (Math.round(Math.random() * 95));
+            targetHori = (Math.round(Math.random() * 47.5));
             score = score + 1;
             // console.log("Score: " + score);
         }
