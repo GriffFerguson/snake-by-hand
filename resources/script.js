277c8c2de1ec6f8f1bac7db1f8db672a179dee34
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-13 15:14:42 -0500

    Added collisions
    Keep working on collision system; game is nearly complete.
    Plan for a port to Bored@school

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,17 @@
 window.onload = function() {
+var score = 0;
+
+//Player
 var horizontal = 49.5;
 var horizontalChange = 0;
 var vertical = 49.5;
 var verticalChange = 0;
-
 var player = document.getElementById("player");
+
+//Target
+var targetHori = (Math.random() * 98);
+var target = document.getElementById("target");
+var targetVert = (Math.random() * 98);
 
 document.addEventListener('keydown', function(e) {
     switch(e.code) {
@@ -34,9 +41,34 @@ document.addEventListener('keydown', function(e) {
 })
 
 setInterval (function() {
+    document.getElementById("score").innerText = score;
+
+    //Player functions
     horizontal = horizontal + horizontalChange;
     vertical = vertical + verticalChange;
     player.style.transform = 'translate(' + horizontal + 'vw,' + vertical + 'vh)';  
-    if (horizontal < 0 || horizontal > 100 || vertical < 0 || vertical > 100) {horizontal = 100;}
+    if (horizontal < 0 || horizontal > 100 || vertical < 0 || vertical > 100) {
+        document.getElementById("gameOver").style.display = 'block';
+        document.getElementById("finalScore").innerText = score;
+        horizontalChange = 0;
+        verticalChange = 0;
+        horizontal = 49.5;
+        vertical = 49.5;
+    }
+
+    //Target functions
+    target.style.transform = 'translate(' + targetHori +'vw, ' + targetVert + 'vh)'
+    if (
+        (horizontal - 2.5) < targetHori &&
+        (horizontal + 2.5) > targetHori &&
+        (vertical + 3.25) > targetVert &&
+        (vertical - 3.25) < targetVert
+        ) {
+            // console.log("Target hit!")
+            targetVert = (Math.random() * 98);
+            targetHori = (Math.random() * 98);
+            score = score + 1;
+            // console.log("Score: " + score);
+        }
 }, 100)
 }
