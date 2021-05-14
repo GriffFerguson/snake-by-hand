0bd4f861f9bdd833ea918077fdbe1495507696bd
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-14 15:52:39 -0500

    Continue working on segmentation

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 var score = 0;
 var gameOver = 0;
+var turnFrame = 0;
 
 //Player
 var horizontal = 50;
@@ -9,10 +10,10 @@ var vertical = 50;
 var verticalChange = 0;
 var player = document.getElementById("player");
 var segmentPosHori = [
-
+    horizontal - 1
 ]
 var segmentPosVert = [
-
+    vertical - 1.75
 ]
 
 //Target
@@ -26,21 +27,31 @@ document.addEventListener('keydown', function(e) {
             // console.log("right arrow pressed");
             horizontalChange = 1;
             verticalChange = 0;
+            turnFrame = 0;
             break;
         case "ArrowLeft":
             // console.log("left arrow pressed");
             horizontalChange = -1;
             verticalChange = 0;
+            turnFrame = 0;
             break;
         case "ArrowUp":
             // console.log("up arrow pressed");
             horizontalChange = 0;
             verticalChange = -1.75;
+            turnFrame = 0;
             break;
         case "ArrowDown":
             // console.log("down arrow pressed");
             horizontalChange = 0;
             verticalChange = 1.75;
+            turnFrame = 0;
+            break;
+        case "Space":
+            horizontalChange = 0;
+            verticalChange = 0;
+            //alert("Game has been paused")
+            console.log("Game movement has been paused")
             break;
     }
     // console.log("Horizontal: " + horizontal);
@@ -48,6 +59,8 @@ document.addEventListener('keydown', function(e) {
 })
 
 setInterval (function() {
+    turnFrame = turnFrame++;
+
     //Player functions
     horizontal = horizontal + horizontalChange;
     vertical = vertical + verticalChange;
@@ -82,26 +95,24 @@ setInterval (function() {
         score = score + 1;
         document.getElementById("score").innerText = score;
         // console.log("Score: " + score);
+
+        //Add player segment
+        var seg = document.createElement("div");
+        seg.classList.add("playersegment");
+        seg.setAttribute("id", "player_seg" + (score - 1));
+        document.body.appendChild(seg);
+        segmentPosHori.push(score - 1);
+        segmentPosVert.push(score - 1);
     }
 
     //Player length
-    segmentPosVert[0] = vertical - (verticalChange * -1);
-    segmentPosHori[0] = horizontal - (horizontalChange * -1);
-    for (let index = 1; index < segmentPosHori.length; index++) {
-        segmentPosHori[index] = segmentPosHori[index - 1];
-        document.getElementById("player_seg" + index).style.left = segmentPosHori[index] + '%';
+    for (let index = 0; index < score; index++) {
+        segmentPosHori[index] = segmentPosHori[index - 1] + horizontalChange;
+        segmentPosVert[index] = segmentPosVert[index - 1] + verticalChange;
     }
-    for (let index = 1; index < segmentPosVert.length; index++) {
-        segmentPosVert[index] = segmentPosVert[index - 1];
+    for (let index = 0; index < (segmentPosHori.length - 1); index++) {
+        document.getElementById("player_seg" + index).style.left = segmentPosHori[index] + '%';
         document.getElementById("player_seg" + index).style.top = segmentPosVert[index] + '%';
     }
-    for (let index = 0; index < score; index++) {
-        var seg = document.createElement("div");
-        seg.classList.add("playersegment");
-        seg.setAttribute("id", "player_seg" + (index + 1));
-        document.body.appendChild(seg);
-        segmentPosHori.push(index + 1);
-        segmentPosVert.push(index + 1);
-    }
-}, 100)
-}
+    console.log("Segment Horizontal: " + segmentPosHori + "\r\n" + "Segment Vertical: " + segmentPosVert);
+}, 500)}
