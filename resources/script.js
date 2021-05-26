b253e1b8e7069e980469b657488735e22b818989
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-26 13:17:39 -0500

    Remake movement system

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,16 @@
+var ar = 0;
+getAspectRatio();
+window.addEventListener('resize', getAspectRatio)
+
+var mspf = 85;
+
 var score = 0;
 var gameOver = 0;
 
 //Player
 var horizontal = 50;
 var horizontalChange = 0;
-var vertical = 50;
+var vertical = (50 * ar);
 var verticalChange = 0;
 var player = document.getElementById("player");
 var horizontalArc = [];
@@ -13,36 +19,36 @@ var verticalArc = [];
 //Target
 var targetHori = (Math.round(Math.random() * 100));
 var target = document.getElementById("target");
-var targetVert = (Math.round(Math.random() * 47.5));
+var targetVert = (Math.round(Math.random() *  (47.5 * ar)));
 
 document.addEventListener('keydown', function(e) {
     switch(e.code) {
         case "ArrowRight":
             // console.log("right arrow pressed");
-            if (horizontalChange != -1.4) {
-                horizontalChange = 1.4;
+            if (horizontalChange != -1.8) {
+                horizontalChange = 1.8;
                 verticalChange = 0;
             }
             break;
         case "ArrowLeft":
             // console.log("left arrow pressed");
-            if (horizontalChange != 1.4) {
-                horizontalChange = -1.4;
+            if (horizontalChange != 1.8) {
+                horizontalChange = -1.8;
                 verticalChange = 0;
             }
             break;
         case "ArrowUp":
             // console.log("up arrow pressed");
-            if (verticalChange != 2) {
+            if (verticalChange != 1.8) {
                 horizontalChange = 0;
-                verticalChange = -2;
+                verticalChange = -1.8;
             }
             break;
         case "ArrowDown":
             // console.log("down arrow pressed");
-            if (verticalChange != -2) {
+            if (verticalChange != -1.8) {
                 horizontalChange = 0;
-                verticalChange = 2;
+                verticalChange = 1.8;
             }
             break;
     }
@@ -54,7 +60,7 @@ setInterval (function() {
     //Player functions
     horizontal = horizontal + horizontalChange;
     vertical = vertical + verticalChange;
-    player.style.top = vertical + 'vh'; 
+    player.style.top = vertical + 'vw'; 
     player.style.left = horizontal + 'vw';
     
     if (horizontal < 0 || horizontal > 100 || vertical < 0 || vertical > 100) {gameOver = 1;}
@@ -65,15 +71,15 @@ setInterval (function() {
         horizontalChange = 0;
         verticalChange = 0;
         horizontal = 50;
-        vertical = 50;
+        vertical = (50 * ar);
         for (let index = 0; index <= score; index++) {
             document.getElementById("player_seg" + index).remove();
         }
     }
 
     //Target functions
-    target.style.top = targetVert + '%';
-    target.style.left = targetHori +'%';
+    target.style.top = targetVert + 'vw';
+    target.style.left = targetHori +'vw';
     if (
         (horizontal - 3) < targetHori &&
         (horizontal + 3) > targetHori &&
@@ -81,8 +87,8 @@ setInterval (function() {
         (vertical - 3) < targetVert
     ) {
         // console.log("Target hit!")
-        targetVert = (Math.round(Math.random() * 95));
-        targetHori = (Math.round(Math.random() * 47.5));
+        targetHori = (Math.round(Math.random() * 100));
+        targetVert = (Math.round(Math.random() *  (47.5 * ar)));
         console.log("targetHori: " + targetHori + "\r\ntargetVert: " + targetVert);
         score = score + 1;
         document.getElementById("score").innerText = score;
@@ -99,8 +105,8 @@ setInterval (function() {
     horizontalArc.unshift(horizontal);
     verticalArc.unshift(vertical);
     for (let index = 0; index < score; index++) {
-        document.getElementById("player_seg" + index).style.left = horizontalArc[index + 1] +'%';
-        document.getElementById("player_seg" + index).style.top = verticalArc[index + 1] + '%';
+        document.getElementById("player_seg" + index).style.left = horizontalArc[index + 1] +'vw';
+        document.getElementById("player_seg" + index).style.top = verticalArc[index + 1] + 'vw';
     }
     if (horizontalArc.length > score) {horizontalArc.pop()}
     if (verticalArc.length > score) {verticalArc.pop()}
@@ -116,8 +122,21 @@ setInterval (function() {
             console.log("Vertical collision");
         }
     }
-}, 85)
+}, mspf)
 
+
+function getAspectRatio() {
+    // Aspect ratio is measured in the 
+    // amount to multiply the horizontal side 
+    // by to reach an equivalent measurement on
+    // the vertical side
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    ar = height/width;
+    console.log("Detected aspect ratio: " + ar);
+}
 
 function reset() {
     var horiArcLength = horizontalArc.length;
@@ -129,7 +148,7 @@ function reset() {
     }
     score = 0;
     horizontal = 50;
-    vertical = 50;
+    vertical = (50 * ar);
     horizontalChange = 0;
     verticalChange = 0;
     gameOver = 0;
